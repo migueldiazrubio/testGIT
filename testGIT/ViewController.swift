152a377a33c1a02af51507f1e359282a83223a93
Miguel Díaz Rubio <info@migueldiazrubio.com>
2017-08-03 19:29:47 +0200

    Initial Commit

---
+++ testGIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGIT
+//
+//  Created by MIGUEL DIAZ RUBIO on 3/8/17.
+//  Copyright © 2017 MIGUEL DIAZ RUBIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
