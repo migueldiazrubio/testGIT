72c67b6f10ef127dba7f45ccbfbf93c6986a229b
Miguel Díaz Rubio <info@migueldiazrubio.com>
2017-08-03 19:35:28 +0200

    Conflicto resuelto

--- testGIT/AppDelegate.swift
+++ testGIT/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         // Comentario en local
+        // Comentario en remoto
         
         return true
     }
